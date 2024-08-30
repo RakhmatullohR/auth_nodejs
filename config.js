d2df8c11d80f58895d89d7175ca4752992b7937e
rakhmatulloh <rrr.engineer.94@gmail.com>
2024-08-30 19:43:46 +0500

    updated

---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  accessTokenSecret: 'jottWkRh0kBswCKr9Wv6En2dnCpaZrBYarIt1ofPe74=',
+};
--- utils/errorHandler.js
+++
@@ -1,8 +0,0 @@
-// utils/errorHandler.js
-const errorHandler = (err, req, res, next) => {
-    console.error('Error:', err.message);
-    res.status(500).json({ error: 'Server error' });
-  };
-  
-  module.exports = errorHandler;
-  
